72f4687d81bc8123b92448e36dac44ce156aec69
bodhidipta <bodhidipta.bhattacharjee@esolzmail.com>
2017-06-29 17:24:02 +0530

    suraj 29 jun

--- app/src/main/java/freewilder/rockme/com/freewilder/fragments/TermsPrivacySignUp.java
+++ app/src/main/java/freewilder/rockme/com/freewilder/fragments/TermsPrivacySignUp.java
@@ -95,7 +95,6 @@ public class TermsPrivacySignUp extends Fragment {
                                 }
                             }
                     );
-
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
