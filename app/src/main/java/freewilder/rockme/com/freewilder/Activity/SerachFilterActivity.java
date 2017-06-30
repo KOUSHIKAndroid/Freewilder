b3091439a4ba34b74cc2b19289c9c1cf49ba9a60
bodhidipta <bodhidipta.bhattacharjee@esolzmail.com>
2017-06-30 13:10:55 +0530

    suraj 30 jun

---
+++ app/src/main/java/freewilder/rockme/com/freewilder/Activity/SerachFilterActivity.java
@@ -0,0 +1,20 @@
+package freewilder.rockme.com.freewilder.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import freewilder.rockme.com.freewilder.R;
+
+/**
+ * Created by su on 6/30/17.
+ */
+
+public class SerachFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_filter);
+    }
+}
